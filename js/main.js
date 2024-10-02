d1dd53b160b0968da2a89f16dd4e25528104c5cf
Magnus Oblerion <82583559+oblerion@users.noreply.github.com>
2024-10-02 12:02:20 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 let button_id=0;
 let projectlist = new ProjectList("project_list");
-projectlist.add("egba-engine","./asset/MlEygI.png","https://oblerion.itch.io/gba-engine",["c"]);
-projectlist.add("elyoko","./asset/dUagsX.png","https://oblerion.itch.io/elyoko",["c"]);
-projectlist.add("love2djs","./asset/Hw4tmq.png","https://oblerion.itch.io/love2djs",["js"]);
-projectlist.add("meca-frelion","./asset/ZvGh+s.png","https://oblerion.itch.io/meca-frelion",["3d"]);
-projectlist.add("matoran","./asset/PmRAtW.png","https://oblerion.itch.io/matoran-model",["3d"]);
-projectlist.add("consoledit","./asset/CxFWHN.png","https://oblerion.itch.io/consoledit",["c","c++"]);
+// 2024
+projectlist.add("egba-engine","./asset/MlEygI.png","https://oblerion.itch.io/gba-engine",["c","2024"]);
+projectlist.add("elyoko","./asset/dUagsX.png","https://oblerion.itch.io/elyoko",["c","2024"]);
+projectlist.add("meca-frelion","./asset/ZvGh+s.png","https://oblerion.itch.io/meca-frelion",["3d","2024"]);
+projectlist.add("matoran","./asset/PmRAtW.png","https://oblerion.itch.io/matoran-model",["3d","2024"]);
+projectlist.add("skyboard","./asset/ClWgNA.png","https://oblerion.itch.io/skyboard-minetest",["lua","3d","2024"]);
+projectlist.add("legion5","./asset/yf5dlD.png","https://oblerion.itch.io/legion5-fox-on-the-run",["lua"]);
+
+// 2023
+projectlist.add("clashounit","./asset/3osmOw.gif","htpps://oblerion.itch.io/clash-of-unit",["lua","2023"]);
+
+// 2022
+projectlist.add("love2djs","./asset/Hw4tmq.png","https://oblerion.itch.io/love2djs",["js","2022"]);
+projectlist.add("anothernight","./asset/M6rGeD.png","https://oblerion.itch.io/another-night",["js","2022"]);
+
+// 2020
+projectlist.add("consoledit","./asset/CxFWHN.png","https://oblerion.itch.io/consoledit",["c","c++","2020"]);
 projectlist.add("voxelpack","./asset/w52W5A.png","https://oblerion.itch.io/3d-voxel-asset",["3d"]);
+projectlist.add("pomodoro","./asset/uTfcGe.png","https://oblerion.itch.io/my-pomodoro-timer",["lua","2020"]);
+
+
 projectlist.filter();
 
 const button_tech=[
@@ -14,6 +28,7 @@ const button_tech=[
 	document.getElementById("btn_C"),
 	document.getElementById("btn_C++"),
 	document.getElementById("btn_JS"),
+	document.getElementById("btn_LUA"),
 	document.getElementById("btn_3D")
 ];
 
@@ -22,6 +37,7 @@ const button_tag=[
 	"c",
 	"c++",
 	"js",
+	"lua",
 	"3d"
 ];
 
